d4533d5009050c3c82a82883346ea5e17da1f221
AnasIbrahim92 <anas.abuhilal.1992@gmail.com>
2017-09-07 04:11:33 +0300

    Initial commit

---
+++ app/src/main/java/mehani/mehani/wyanbu/com/mehani/MainActivity.java
@@ -0,0 +1,61 @@
+package mehani.mehani.wyanbu.com.mehani;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        setContentView(R.layout.activity_main);
+                    }
+                }, 3000);
+
+    }
+
+    public void login(View view) {
+        EditText phone = (EditText) findViewById(R.id.input_phone);
+        if (phone.length() != 10) {
+            phone.setError("Phone num error");
+        } else {
+            setContentView(R.layout.activity_main_2);
+        }
+    }
+
+    public void check(View view) {
+        EditText check = (EditText) findViewById(R.id.input_check);
+
+        if (check.length() != 6) {
+            check.setError("Check Code Error");
+        } else {
+            setContentView(R.layout.activity_main_3);
+
+            String[] items = new String[]{"a1", "a2", "a3"};
+
+            Spinner spinner = (Spinner) findViewById(R.id.spinner_city);
+
+            ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    items);
+            spinner.setAdapter(spinnerArrayAdapter);
+
+
+
+
+        }
+    }
+
+    public void done(View view) {
+
+    }
+}
