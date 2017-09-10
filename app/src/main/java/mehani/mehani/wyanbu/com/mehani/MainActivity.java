1842e6718e5daeeb0f7865ffaa9ecda9e256630e
AnasIbrahim92 <anas.abuhilal.1992@gmail.com>
2017-09-11 01:55:11 +0300

    10-09-2017/2.28 AM

--- app/src/main/java/mehani/mehani/wyanbu/com/mehani/MainActivity.java
+++ app/src/main/java/mehani/mehani/wyanbu/com/mehani/MainActivity.java
@@ -1,7 +1,9 @@
 package mehani.mehani.wyanbu.com.mehani;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
@@ -10,6 +12,22 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     String[] items = new String[]{"a1", "a2", "a3"};
@@ -25,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 new Runnable() {
                     public void run() {
 
-                        if (false) {
+                        if (true) {
                             setContentView(R.layout.activity_main);
                             EditText phone = (EditText) findViewById(R.id.input_phone);
                             phone.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -79,6 +97,67 @@ public class MainActivity extends AppCompatActivity {
     private void check_fun() {
         EditText check = (EditText) findViewById(R.id.input_check);
 
+        //test
+
+
+        final ProgressDialog progress = new ProgressDialog(this);
+        progress.setTitle("Loading");
+        progress.setCancelable(false);
+        progress.show();
+
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, network.a5,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        String test = "1";
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            test = json.getString("Success");
+                            if(test.equals("1")){
+                                test = json.getString("Exists");
+                                if(test.equals("1")){
+                                    test = json.getString("Errors");
+                                    Toast.makeText(getApplicationContext(),"error "+test,Toast.LENGTH_LONG).show();
+                            }else {
+                                    Toast.makeText(getApplicationContext(),"exists "+test,Toast.LENGTH_LONG).show();}
+                            }else {
+                            Toast.makeText(getApplicationContext(),test,Toast.LENGTH_LONG).show();}
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("mobile", number+"");
+
+                return params;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(postRequest);
+
+
+        startActivity(new Intent(this, Home.class));
+        finish();
+
+
         if (check.length() != 6) {
             check.setError("Check Code Error");
         } else {
@@ -95,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
             });
             Spinner spinner = (Spinner) findViewById(R.id.spinner_city);
 
-            ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,items);
+            ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items);
             spinner.setAdapter(spinnerArrayAdapter);
         }
     }
--- app/src/main/java/mehani/mehani/wyanbu/com/mehani/network.java
+++ app/src/main/java/mehani/mehani/wyanbu/com/mehani/network.java
@@ -9,6 +9,7 @@ public class network {
     public static final String a2="http://mehani.wyanbu.com/Json/GetFields/";
     public static final String a3="http://mehani.wyanbu.com/Json/SetOrder";
     public static final String a4="http://mehani.wyanbu.com/Json/GetOrders";
+    public static final String a5="http://mehani.wyanbu.com/Json/Register";
 
 
 }
